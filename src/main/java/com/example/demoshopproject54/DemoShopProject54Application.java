e6522899b0861f408ca58c608377e4bf256468ea
Madalin <madalin_bleoca@yahoo.com>
2023-09-23 13:32:47 +0300

    start app

---
+++ src/main/java/com/example/demoshopproject54/DemoShopProject54Application.java
@@ -0,0 +1,13 @@
+package com.example.demoshopproject54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoShopProject54Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoShopProject54Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoshopproject54/DemoShopProject54ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoshopproject54;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoShopProject54ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
